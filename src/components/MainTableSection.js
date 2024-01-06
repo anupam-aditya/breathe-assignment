0a70a8fa2de370d840348c98f09e3afdbf8c1880
anupam-aditya <anupamaditya99@gmail.com>
2024-01-06 19:05:27 +0530

    tracker added

--- src/components/MainTableSection.js
+++ src/components/MainTableSection.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 import { BsShare } from "react-icons/bs";
 import { IoChevronDown } from "react-icons/io5";
 import { RiDeleteBinLine } from "react-icons/ri";
@@ -6,115 +6,206 @@ import { BiBuilding } from "react-icons/bi";
 import Profile from "./Profile";
 import Tracker from "./Tracker";
 
-const MainTableSection = (props)=>{
-    const [tracker, setTracker] = useState("tracker");
-    const trackerToggler = (e)=>{
-        if(e.target.innerText === "Data Entry"){
-            setTracker("tracker");
-        }
-        else{
-            setTracker(null);
-        }
-    }
+const MainTableSection = (props) => {
+	const [tracker, setTracker] = useState("tracker");
+	const trackerToggler = (e) => {
+		if (e.target.innerText === "Data Entry") {
+			setTracker("tracker");
+		} else {
+			setTracker(null);
+		}
+	};
 
-    return (
-    <div style={{background:"white", color:"#000", display:"flex", flexDirection:"column", flex:"1", alignItems:"start"}}>
-      <div style={{display:"flex", flexDirection:"column", gap:"20px", padding:"24px", width:"96.8%"}}>
-        <Profile />
-        <div style={{display:"flex", justifyContent:"space-between"}}>
-          <div style={{display:"flex", gap:"48px"}}>
-            <p style={{display:"flex", alignItems:"center", gap:"4px"}} onClick={trackerToggler}><BiBuilding />Data Entry</p>
-            <p style={{display:"flex", alignItems:"center", gap:"4px"}} onClick={trackerToggler}><BiBuilding />Tracker</p>
-          </div>
-          <div style={{display:"flex", gap:"16px"}}>
-            <p style={{display:"flex", gap:"12px", margin:"0px", alignItems:"center"}}>For: <span style={{color:"#000", borderRadius:"4px", padding:"8px 16px", border:"1px solid #C9C9C9"}}>FY 2023-24 <IoChevronDown/></span></p>
-            <button style={{background:"#2E9844", borderRadius:"4px", padding:"8px 16px", color:"white", border:"1px solid", outline:"none", fontSize:"15px",fontWeight:"700"}}>Submit For Approval</button>
-          </div>
-        </div>
-      </div>
-      {tracker?(<div style={{width:"100%"}}>
-      <table style={{width:"100%"}}>
-        <tbody>
-      <tr className="table-row">
-        <th><input type="checkbox" className="table-input"/></th>
-        <th>ASSESSMENT TITLE</th>
-        <th>TYPE</th>
-        <th>NO. OF SUPPLIERS</th>
-        <th>SCORE</th>
-        <th>RISK CLASSIFICATION</th>
-        <th>STATUS</th>
-        <th>RESULT</th>
-        <th>ACTIONS</th>
-      </tr>
-      <tr className="table-row">
-        <td>
-        <input type="checkbox" className="table-input"/>
-        </td>
-        <td>Assessment 1</td>
-        <td>BRSR</td>
-        <td>20</td>
-        <td>-</td>
-        <td><span style={{color: "#f1c40f"}}>Medium</span></td>
-        <td>Pending</td>
-        <td>-</td>
-        <td className="table-actions"><BsShare /><RiDeleteBinLine /></td>
-      </tr>
-      <tr className="table-row">
-        <td>
-        <input type="checkbox" className="table-input"/>
-        </td>
-        <td>Assessment 2</td>
-        <td>BRSR</td>
-        <td>25</td>
-        <td>98</td>
-        <td><span style={{color: "#f1c40f"}}>Low</span></td>
-        <td>Complete</td>
-        <td>View</td>
-        <td className="table-actions"><BsShare /><RiDeleteBinLine /></td>
-      </tr>
-      <tr className="table-row">
-        <td>
-        <input type="checkbox" className="table-input"/>
-        </td>
-        <td>Assessment 3</td>
-        <td>BRSR</td>
-        <td>35</td>
-        <td>22</td>
-        <td><span style={{color: "#f1c40f"}}>High</span></td>
-        <td>Complete</td>
-        <td>View</td>
-        <td className="table-actions"><BsShare /><RiDeleteBinLine /></td>
-      </tr>
-      <tr className="table-row">
-        <td>
-        <input type="checkbox" className="table-input"/>
-        </td>
-        <td>Assessment 3</td>
-        <td>Custom</td>
-        <td>49</td>
-        <td>23</td>
-        <td><span style={{color: "#f1c40f"}}>Medium</span></td>
-        <td>Complete</td>
-        <td>View</td>
-        <td className="table-actions"><BsShare /><RiDeleteBinLine /></td>
-      </tr>
-      <tr className="table-row">
-        <td>
-        <input type="checkbox" className="table-input"/>
-        </td>
-        <td>Assessment 3</td>
-        <td>Custom</td>
-        <td>100</td>
-        <td>42</td>
-        <td><span style={{color: "#f1c40f"}}>Medium</span></td>
-        <td>Complete</td>
-        <td>View</td>
-        <td className="table-actions"><BsShare /><RiDeleteBinLine /></td>
-      </tr>
-      </tbody>
-    </table>
-  
-      </div>):(<Tracker />)}
-    </div>)
-  }
-  export default MainTableSection;
+	return (
+		<div
+			style={{
+				background: "white",
+				color: "#000",
+				display: "flex",
+				flexDirection: "column",
+				flex: "1",
+				alignItems: "start",
+			}}
+		>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					gap: "20px",
+					padding: "24px",
+					width: "-webkit-fill-available",
+				}}
+			>
+				<Profile />
+				<div style={{ display: "flex", justifyContent: "space-between" }}>
+					<div style={{ display: "flex", gap: "48px" }}>
+						<p
+							style={{ display: "flex", alignItems: "center", gap: "4px" }}
+							onClick={trackerToggler}
+						>
+							<BiBuilding />
+							Data Entry
+						</p>
+						<p
+							style={{ display: "flex", alignItems: "center", gap: "4px" }}
+							onClick={trackerToggler}
+						>
+							<BiBuilding />
+							Tracker
+						</p>
+					</div>
+					<div style={{ display: "flex", gap: "16px" }}>
+						<p
+							style={{
+								display: "flex",
+								gap: "12px",
+								margin: "0px",
+								alignItems: "center",
+							}}
+						>
+							For:{" "}
+							<span
+								style={{
+									color: "#000",
+									borderRadius: "4px",
+									padding: "8px 16px",
+									border: "1px solid #C9C9C9",
+								}}
+							>
+								FY 2023-24 <IoChevronDown />
+							</span>
+						</p>
+						<button
+							style={{
+								background: "#2E9844",
+								borderRadius: "4px",
+								padding: "8px 16px",
+								color: "white",
+								border: "1px solid",
+								outline: "none",
+								fontSize: "15px",
+								fontWeight: "700",
+							}}
+						>
+							Submit For Approval
+						</button>
+					</div>
+				</div>
+			</div>
+			{tracker ? (
+				<div style={{ width: "100%" }}>
+					<table style={{ width: "100%" }}>
+						<tbody>
+							<tr className="table-row">
+								<th>
+									<input type="checkbox" className="table-input" />
+								</th>
+								<th>ASSESSMENT TITLE</th>
+								<th>TYPE</th>
+								<th>NO. OF SUPPLIERS</th>
+								<th>SCORE</th>
+								<th>RISK CLASSIFICATION</th>
+								<th>STATUS</th>
+								<th>RESULT</th>
+								<th>ACTIONS</th>
+							</tr>
+							<tr className="table-row">
+								<td>
+									<input type="checkbox" className="table-input" />
+								</td>
+								<td>Assessment 1</td>
+								<td>BRSR</td>
+								<td>20</td>
+								<td>-</td>
+								<td>
+									<span style={{ color: "#f1c40f" }}>Medium</span>
+								</td>
+								<td>Pending</td>
+								<td>-</td>
+								<td className="table-actions">
+									<BsShare />
+									<RiDeleteBinLine />
+								</td>
+							</tr>
+							<tr className="table-row">
+								<td>
+									<input type="checkbox" className="table-input" />
+								</td>
+								<td>Assessment 2</td>
+								<td>BRSR</td>
+								<td>25</td>
+								<td>98</td>
+								<td>
+									<span style={{ color: "#f1c40f" }}>Low</span>
+								</td>
+								<td>Complete</td>
+								<td>View</td>
+								<td className="table-actions">
+									<BsShare />
+									<RiDeleteBinLine />
+								</td>
+							</tr>
+							<tr className="table-row">
+								<td>
+									<input type="checkbox" className="table-input" />
+								</td>
+								<td>Assessment 3</td>
+								<td>BRSR</td>
+								<td>35</td>
+								<td>22</td>
+								<td>
+									<span style={{ color: "#f1c40f" }}>High</span>
+								</td>
+								<td>Complete</td>
+								<td>View</td>
+								<td className="table-actions">
+									<BsShare />
+									<RiDeleteBinLine />
+								</td>
+							</tr>
+							<tr className="table-row">
+								<td>
+									<input type="checkbox" className="table-input" />
+								</td>
+								<td>Assessment 3</td>
+								<td>Custom</td>
+								<td>49</td>
+								<td>23</td>
+								<td>
+									<span style={{ color: "#f1c40f" }}>Medium</span>
+								</td>
+								<td>Complete</td>
+								<td>View</td>
+								<td className="table-actions">
+									<BsShare />
+									<RiDeleteBinLine />
+								</td>
+							</tr>
+							<tr className="table-row">
+								<td>
+									<input type="checkbox" className="table-input" />
+								</td>
+								<td>Assessment 3</td>
+								<td>Custom</td>
+								<td>100</td>
+								<td>42</td>
+								<td>
+									<span style={{ color: "#f1c40f" }}>Medium</span>
+								</td>
+								<td>Complete</td>
+								<td>View</td>
+								<td className="table-actions">
+									<BsShare />
+									<RiDeleteBinLine />
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			) : (
+				<Tracker />
+			)}
+		</div>
+	);
+};
+export default MainTableSection;
--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,52 +1,127 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import progress from "../progress.svg";
+import plus from "../plus.svg";
+import { FaLongArrowAltDown } from "react-icons/fa";
+import { IoIosFunnel } from "react-icons/io";
 
 const data = [
-  { month: 'Jan 2023', status: 'PENDING', completion: 20, unit: 'Business Unit 1' },
-  { month: 'Feb 2023', status: 'APPROVED', completion: 30, unit: 'Business Unit 1' },
-  { month: 'Mar 2023', status: 'INCOMPLETE', completion: 50, unit: 'Business Unit 1' },
+	{
+		month: "Jan 2023",
+		status: "PENDING",
+		completion: 20,
+		unit: "Business Unit 1",
+	},
+	{
+		month: "Feb 2023",
+		status: "APPROVED",
+		completion: 30,
+		unit: "Business Unit 1",
+	},
+	{
+		month: "Mar 2023",
+		status: "INCOMPLETE",
+		completion: 50,
+		unit: "Business Unit 1",
+	},
 ];
-const Tracker = ()=>{
-    const [filteredData, setFilteredData] = useState(data);
+const Tracker = () => {
+	const [filteredData, setFilteredData] = useState(data);
 
-    const handleSearch = (event) => {
-    const value = event.target.value.toLowerCase();
-    const filtered = data.filter((item) =>
-      item.unit.toLowerCase().includes(value) ||
-      item.status.toLowerCase().includes(value) ||
-      item.month.toLowerCase().includes(value)
-    );
-    setFilteredData(filtered);
-  };
+	const handleSearch = (event) => {
+		const value = event.target.value.toLowerCase();
+		const filtered = data.filter(
+			(item) =>
+				item.unit.toLowerCase().includes(value) ||
+				item.status.toLowerCase().includes(value) ||
+				item.month.toLowerCase().includes(value)
+		);
+		setFilteredData(filtered);
+	};
 
-  return (
-    <div className="tracker-main-section">
-      <div className="header">
-        <div>Pending Trackers</div>
-        <div>Pending Reviews</div>
-      </div>
-      <input type="search" placeholder="Search for a business unit" onChange={handleSearch} />
-      <table>
-        <thead>
-          <tr>
-            <th>Month</th>
-            <th>Status</th>
-            <th>Completion %</th>
-            <th>Business Unit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((item, index) => (
-            <tr key={index}>
-              <td>{item.month}</td>
-              <td>{item.status}</td>
-              <td>{item.completion}%</td>
-              <td>{item.unit}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}
+	return (
+		<div className="tracker-main-section">
+			<div className="d-flex" style={{ gap: "14px" }}>
+				<div className="d-flex" style={{ gap: "16px", alignItems: "start" }}>
+					<p className="d-flex-column" style={{ margin: "0" }}>
+						<span>Pending Trackers</span>
+						<span>45/60</span>
+					</p>
+					<img src={progress}></img>
+				</div>
+				<div className="d-flex" style={{ gap: "16px", alignItems: "start" }}>
+					<p className="d-flex-column" style={{ margin: "0" }}>
+						<span>Pending Reviews</span>
+						<span>3</span>
+					</p>
+					<img src={plus}></img>
+				</div>
+			</div>
+			<input
+				type="search"
+				placeholder="Search for a business unit"
+				onChange={handleSearch}
+			/>
+			<div
+				style={{
+					display: "flex",
+					padding: "24px",
+					justifyContent: "space-between",
+				}}
+			>
+				<div className="tracker-row">
+					<div>
+						Month <FaLongArrowAltDown />
+					</div>
+					<div>Jan 2023</div>
+					<div>Feb 2023</div>
+					<div>Mar 2023</div>
+				</div>
+				<div className="tracker-row">
+					<div>
+						Status <FaLongArrowAltDown />
+					</div>
+					<div>Jan 2023</div>
+					<div>Feb 2023</div>
+					<div>Mar 2023</div>
+				</div>
+				<div className="tracker-row">
+					<div>
+						Completion% <FaLongArrowAltDown />
+					</div>
+					<div>Jan 2023</div>
+					<div>Feb 2023</div>
+					<div>Mar 2023</div>
+				</div>
+				<div className="tracker-row">
+					<div>
+						Business Unit <IoIosFunnel />
+					</div>
+					<div>Business Unit 1</div>
+					<div>Business Unit 1</div>
+					<div>Business Unit 1</div>
+				</div>
+			</div>
+			{/* <table style={{ width: "100%", padding: "24px" }}>
+				<thead>
+					<tr>
+						<th>Month</th>
+						<th>Status</th>
+						<th>Completion %</th>
+						<th>Business Unit</th>
+					</tr>
+				</thead>
+				<tbody>
+					{filteredData.map((item, index) => (
+						<tr key={index}>
+							<td>{item.month}</td>
+							<td>{item.status}</td>
+							<td>{item.completion}%</td>
+							<td>{item.unit}</td>
+						</tr>
+					))}
+				</tbody>
+			</table> */}
+		</div>
+	);
+};
 export default Tracker;
